f55a7bebc7a54da020d905da20b4f66795ed6fcf
Vanessa <vanessa.3bl@gmail.com>
2023-01-25 14:39:41 +0000

    Complete app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'dfc3b4da37mshbb2f425d67d7eb0p15b602jsne7e08067df70',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "792b3a3316dbb11b2bf25aacf845b93a";
