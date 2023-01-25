5a010dae95a6484ec1f4987100dd43e1dee59197
Vanessa Ávila <104768121+VanessaJAvila@users.noreply.github.com>
2023-01-25 15:14:36 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'dfc3b4da37mshbb2f425d67d7eb0p15b602jsne7e08067df70',
+        'X-RapidAPI-Key': 'key here',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
 
-export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo";
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "792b3a3316dbb11b2bf25aacf845b93a";
+export const GEO_API_URL = "geo api url here";
+export const WEATHER_API_URL = "api url here";
+export const WEATHER_API_KEY = "your key here";
